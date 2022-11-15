78841371b464ecbe191dfdfbb660caaa7fde90b6
abigail.gbadago <afimaamedufie@gmail.com>
2022-11-15 09:10:23 +0000

    Testing out precommits feat check

---
+++ sample2.py
@@ -0,0 +1,21 @@
+"""Demonstration of Pre Commits"""
+
+
+def mul(        a, b):
+    res = a *          b
+
+    return res
+
+
+
+
+
+
+
+
+def mul(x,     y):
+    if x >       y: 
+        res = x % y
+        return res
+    else:
+        return x,                  y
