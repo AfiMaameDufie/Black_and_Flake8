494b444b35b0df011a1648435f2380f41176dca5
abigail.gbadago <afimaamedufie@gmail.com>
2022-11-15 09:35:09 +0000

    Testing precommit check  feat

---
+++ sample1.py
@@ -0,0 +1,13 @@
+"""Demonstrating how Black neatens code"""
+
+def add(       a, b):
+    res = a + b
+
+    return res
+
+
+def sub(c, d):
+    res = c -        d
+    print('Subtraction done!!!')
+
+    return res
